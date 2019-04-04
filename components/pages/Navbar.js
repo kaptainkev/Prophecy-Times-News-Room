364abc9e9b62d539f108e3b6681fa61bb1e6f128
kaptainkev <kaptainkev@gmail.com>
2019-04-04 11:22:15 -0400

    1 file modified

--- components/pages/Navbar.js
+++ components/pages/Navbar.js
@@ -50,6 +50,12 @@ const Navbar = (props) =>{
   
 </div>
 
+<nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+
 
 
 
