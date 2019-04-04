35ee8aca8c9204cf2d7d5589f014524546ab4d57
kaptainkev <kaptainkev@gmail.com>
2019-04-04 02:10:10 -0400

    2 files modified

--- components/pages/Navbar.js
+++ components/pages/Navbar.js
@@ -16,7 +16,9 @@ const Navbar = (props) =>{
       <div className = "container">
         <div className="glyphicon glyphicon-plane"></div>
           <Link  to="/" className="brand-logo left font">
-          <p className="PNews shadowLogo">P-News <i class="material-icons right">money</i>
+          <p className="PNews ">P-News
+<i class="fa fa-newspaper-o shadowLogo right "></i>
+
           </p>
 
          </Link>
