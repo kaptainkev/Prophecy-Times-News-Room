58a4394cbf78a3f948cba3582760b71ba699ae6b
kaptainkev <kaptainkev@gmail.com>
2019-04-08 10:14:59 -0400

    2 files modified

--- components/pages/Navbar.js
+++ components/pages/Navbar.js
@@ -12,8 +12,8 @@ const Navbar = (props) =>{
 
   return (
     <div>
-     <nav className="navbarcontainer">
-      <div className = "container">
+     <nav >
+      <div className = "nav-wrapper">
         <div className="glyphicon glyphicon-plane"></div>
           <Link  to="/" className="brand-logo left font">
           <p className="PNews ">P-NEWS 
@@ -26,9 +26,10 @@ const Navbar = (props) =>{
 
 
 
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><Link to="/contact">Timeline</Link></li>
-            <li><Link to="/article">News Room</Link></li>
+            <li><NavLink className="" to="/about">About <i className="material-icons right">chrome_reader_mode
+</i></NavLink></li>
+            <li><Link className="" to="/contact">Timeline <i className="material-icons right  ">timelapse</i></Link></li>
+            <li><Link className="" to="/article">News Room <i className="material-icons right">cast_connected</i></Link></li>
 
           </ul>
         </div>
