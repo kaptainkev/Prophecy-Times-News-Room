eeb07b52ecb1c7b9c4b31013131b7ba883d3f8b1
kaptainkev <kaptainkev@gmail.com>
2019-04-10 17:11:45 -0400

    1 file added

---
+++ components/services/newsAPI/newsAPI.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {router, Route, IndexRoute, hashHistory} from "react-router"; 
+import axios from 'axios';
+
+
+
+export default class ArticleView extends Component {
+  constructor() {
+    super();
+    this.state = {search: ""};
+
+}
+
+
+componentDidMount()
+{
+    this.GetNewsAPIData("");
+}
+
+GetNewsAPIData(searchVal)
+{
+  //axios.get('https://jsonplaceholder.typicode.com/todos/1/posts').then(res => {
+  //console.log(res)
+  //this.setState({ posts: res.data.slice(0,10) })
+this.setState({search: searchVal});
+
+ var accessUrl = 'https://newsapi.org/v2/everything?q=' + searchVal + '&domains=wsj.com&apiKey=f129749be7d6479c802c840bd952da97';
+ //accessUrl = "'https://newsapi.org/v2";
+  axios({
+        method: 'get',
+        url: accessUrl,
+        
+      })
+    .then(function (data) {
+      //console.log('Success ' + JSON.stringify(data))
+      this.setState({posted: data});
+      console.log(this.state.posted);
+
+    }.bind(this))
+    .catch(function (error) {
+      console.log('Error ' + error.message)
+    });       
+
+}
+}
+
